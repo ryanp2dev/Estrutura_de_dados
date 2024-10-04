1340542262280513ca9ef8e1ccc89380e2ea8b65
ryanp2dev <65263907+ryanp2dev@users.noreply.github.com>
2024-10-03 22:36:29 -0400

    Update Vagao.java

--- Filha/src/filha/Vagao.java
+++ Filha/src/filha/Vagao.java
@@ -16,17 +16,23 @@ public class Vagao {
         
         
     public static void main (String args[]) {
+       Vagao maquinista = new Vagao();
+       maquinista = null;
+        
+        
+        
         Vagao v1 = new Vagao();
         v1.rodas = 4;
         v1.janelas = 2;
         System.out.println(v1.toString());
-    
+      
         
         Vagao v2 = new Vagao();
         v2.rodas = 6;
         v2.janelas = 4;
         System.out.println(v2.toString());
         v2.prox = v1;
+        maquinista = v2;
          System.out.println(v2.toString());
 }
 
